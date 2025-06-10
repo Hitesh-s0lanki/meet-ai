6dc6097a4fbc2eccdd42899479f9df3e2d5289c5
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-06-10 15:58:14 +0530

    -- meeting setup done -- create, update, start, meeting

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
 type Props = {
+  image?: string;
   title: string;
   description: string;
 };
 
-export function EmptyState({ title, description }: Props) {
+export function EmptyState({ title, description, image }: Props) {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Image src="/empty.svg" alt="Empty" width={240} height={240} />
+      <Image src={image || "/empty.svg"} alt="Empty" width={240} height={240} />
       <div className="mx-auto flex max-w-md flex-col gap-y-6 text-center">
         <h6 className="text-lg font-medium">{title}</h6>
         <p className="text-muted-foreground text-sm">{description}</p>
---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { DataTable } from "@/components/data-table";
+import { EmptyState } from "@/components/empty-state";
+import { DataPagination } from "@/components/data-pagination";
+
+import { columns } from "../components/columns";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+
+export const MeetingsView = () => {
+  const router = useRouter();
+  const [filters, setFilters] = useMeetingsFilters();
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getMany.queryOptions({ ...filters })
+  );
+
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/meetings/${row.id}`)}
+      />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => {
+          setFilters({ page });
+          // router.push(`?${new URLSearchParams({ page: page.toString() })}`)
+        }}
+      />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Schedule a meeting to connect with others. Each meeting let you collaborate, share ideas, and interact with participants in real-time."
+        />
+      )}
+    </div>
+  );
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="Please wait while we load the Meetings"
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Something went wrong"
+      description="Please try again later"
+    />
+  );
+};
