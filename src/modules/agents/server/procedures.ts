5e17c15039fabcc45ea1fd3a20d367447cac9e14
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-06-10 12:29:41 +0530

    - agent setup - agent page - agent api's

---
+++ src/modules/agents/server/procedures.ts
@@ -0,0 +1,102 @@
+import { TRPCError } from '@trpc/server';
+import { and, count, desc, eq, getTableColumns, ilike } from 'drizzle-orm';
+import { z } from 'zod';
+
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from '@/constants';
+import { db } from '@/db';
+import { agents } from '@/db/schema/agent';
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
+import { agentsInsertSchema, agentsUpdateSchema } from '../schemas';
+
+export const agentsRouter = createTRPCRouter({
+  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
+    const [existingAgent] = await db
+      .select({
+        ...getTableColumns(agents),
+        meetingCount: db.$count(agents, eq(agents.userId, ctx.auth.session.userId)),
+      })
+      .from(agents)
+      .where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)));
+
+    if (!existingAgent) {
+      throw new TRPCError({ code: 'NOT_FOUND', message: 'Agent not fount' });
+    }
+
+    return existingAgent;
+  }),
+  getMany: protectedProcedure
+    .input(
+      z
+        .object({
+          page: z.number().default(DEFAULT_PAGE),
+          pageSize: z.number().min(MIN_PAGE_SIZE).max(MAX_PAGE_SIZE).default(DEFAULT_PAGE_SIZE),
+          search: z.string().nullish(),
+        })
+        .default({}),
+    )
+    .query(async ({ input, ctx }) => {
+      const { page, pageSize, search } = input;
+      const data = await db
+        .select({
+          ...getTableColumns(agents),
+          meetingCount: db.$count(agents, eq(agents.userId, ctx.auth.session.userId)),
+        })
+        .from(agents)
+        .where(
+          and(
+            eq(agents.userId, ctx.auth.session.userId),
+            search ? ilike(agents.name, `%${search}%`) : undefined,
+          ),
+        )
+        .orderBy(desc(agents.createdAt), desc(agents.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize);
+
+      const [total] = await db
+        .select({ count: count() })
+        .from(agents)
+        .where(
+          and(
+            eq(agents.userId, ctx.auth.session.userId),
+            search ? ilike(agents.name, `%${search}%`) : undefined,
+          ),
+        );
+      const totalPages = Math.ceil(total.count / pageSize);
+
+      return {
+        totalPages,
+        total: total.count,
+        items: data,
+      };
+    }),
+  create: protectedProcedure.input(agentsInsertSchema).mutation(async ({ input, ctx }) => {
+    const [createdAgent] = await db
+      .insert(agents)
+      .values({ ...input, userId: ctx.auth.user.id })
+      .returning();
+    return createdAgent;
+  }),
+  update: protectedProcedure.input(agentsUpdateSchema).mutation(async ({ input, ctx }) => {
+    const [updatedAgent] = await db
+      .update(agents)
+      .set(input)
+      .where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)))
+      .returning();
+    if (!updatedAgent) {
+      throw new TRPCError({ code: 'NOT_FOUND', message: 'Agent not found' });
+    }
+    return updatedAgent;
+  }),
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const [removedAgent] = await db
+        .delete(agents)
+        .where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)))
+        .returning();
+      if (!removedAgent) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Agent not found' });
+      }
+      return removedAgent;
+    }),
+});
---
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { DEFAULT_PAGE } from "@/constants";
+import { useAgentFilter } from "../../hooks/use-agents-filter";
+import { AgentSearchFilter } from "./agent-search-filter";
+import { NewAgentDialog } from "./new-agent-dialog";
+
+export function AgentsListHeader() {
+  const [filters, setFilters] = useAgentFilter();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const isAnyFilterModidified = !!filters.search;
+
+  const onClearFilter = () => {
+    setFilters({
+      search: "",
+      page: DEFAULT_PAGE,
+    });
+  };
+
+  return (
+    <>
+      <NewAgentDialog open={isDialogOpen} onOpenAction={setIsDialogOpen} />
+      <div className="flex flex-col gap-y-4 p-4 md:px-8">
+        <div className="flex items-center justify-between">
+          <h5 className="text-xl font-medium">My agents</h5>
+          <Button onClick={() => setIsDialogOpen(true)}>
+            <PlusIcon />
+            New Agent
+          </Button>
+        </div>
+        <div className="item-center flex gap-x-2 p-1">
+          <AgentSearchFilter />
+          {isAnyFilterModidified && (
+            <Button variant="outline" onClick={onClearFilter}>
+              <XCircleIcon />
+              Clear
+            </Button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
