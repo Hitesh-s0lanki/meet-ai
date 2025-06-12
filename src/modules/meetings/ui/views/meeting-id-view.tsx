2a0b19709756b353e6cf6d1f50203435ae963d97
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-06-12 11:46:31 +0530

    -- meeting view page completed

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -20,6 +20,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import CompletedState from "../components/completed-state";
 
 type MeetingIdViewProps = { meetingId: string };
 
@@ -83,7 +84,7 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
           onRemove={handleRemoveMeeting}
         />
         {isCancelled && <CancelledState />}
-        {isCompleted && <div>Completed</div>}
+        {isCompleted && <CompletedState data={data} />}
         {isProcessing && <ProcessingState />}
         {isUpcoming && (
           <UpcomingState
